7581cfd4a90a708a2d1de0a3ec81e62ae7053fef
srihan93 <srihan93@gmail.com>
2021-05-30 19:23:10 +0530

    added validation file

--- apis/_login.js
+++ apis/_login.js
@@ -1,8 +1,17 @@
 var authorize = require('./../lib/authorize');
 
+//try to send the error code from here
+
 _login.post = function(data,callback)
 {
-    authorize.createToken(data,callback);
+    authorize.createToken(data,(err)=>
+    {
+        if(err === false)
+        callback(401);
+        else
+        callback(data);
+
+    });
 }
 
 module.exports = _login;
--- apis/_logout.js
+++ apis/_logout.js
@@ -1,5 +1,17 @@
+var authorize = require('./../lib/authorize');
 
+//try to send the error code or response body from here the output can be defined at API levels - current folder
 
+_logout.post = function(data,callback)
+{
+    authorize.deleteToken(data,(err)=>
+    {
+        if(err === false)
+        callback(401);
+        else
+        callback(data);
 
+    });
+}
 
 module.exports = _logout;
