2b47b1b1d11401aee73d557cdc8abbfebdeaeec7
srihan93 <srihan93@gmail.com>
2021-05-31 22:39:58 +0530

    gitignore and removed unknown objects from handlers

--- apis/_login.js
+++ apis/_login.js
@@ -1,5 +1,7 @@
 var authorize = require('./../lib/authorize');
 
+
+var _login = {}
 //try to send the error code from here
 
 _login.post = function(data,callback)
--- apis/_user.js
+++ apis/_user.js
@@ -1,10 +1,29 @@
-var helpers = require('../lib/helpers');
+const data = require("../lib/data");
+var helpers = require("../lib/helpers");
 
 var _user = {};
 
-_user.post = function()
-{
-    
-}
+_user.post = function (data, callback) {
+  var firstName =
+    typeof data.body.firstName == "string" && data.body.firstName.trim() > 0
+      ? data.body.firstName.trim()
+      : false;
+  var lastName =
+    typeof data.body.lastName == "string" && data.body.lastName.trim() > 0
+      ? data.body.lastName.trim()
+      : false;
+  var email =
+    typeof data.body.email == "string" && data.body.email.trim() > 0
+      ? data.body.email.trim()
+      : false;
+  var phone =
+    typeof data.body.phone == "number" && data.body.phone > 0
+      ? data.body.phone
+      : false;
+  if (firstName && lastName && email && phone) {
+  } else {
+    callback(true);
+  }
+};
 
 module.exports = _user;
