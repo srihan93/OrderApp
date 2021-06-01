0c95fbc85b222d56637b89e738b480d6af58b095
srihan93 <srihan93@gmail.com>
2021-06-01 23:02:38 +0530

    added functionilty to create users

--- apis/_user.js
+++ apis/_user.js
@@ -1,26 +1,30 @@
+const db = require("../lib/db");
 var helpers = require("../lib/helpers");
+var validation  = require('./../lib/validation');
 
+//callback methods return err as main parameter, so setting err=true is the call is having some failure
+var collection ="users";
 var _user = {};
 
 _user.post = function (data, callback) {
-  var firstName =
-    typeof data.body.firstName == "string" && data.body.firstName.trim() > 0
-      ? data.body.firstName.trim()
-      : false;
-  var lastName =
-    typeof data.body.lastName == "string" && data.body.lastName.trim() > 0
-      ? data.body.lastName.trim()
-      : false;
-  var email =
-    typeof data.body.email == "string" && data.body.email.trim() > 0
-      ? data.body.email.trim()
-      : false;
-  var phone =
-    typeof data.body.phone == "number" && data.body.phone > 0
-      ? data.body.phone
-      : false;
-  if (firstName && lastName && email && phone) {
-  } else {
+  if(validation.userCreation(data))
+  {
+      var hashedPassword = helpers.hash(data.body.password);
+      data.body.password = hashedPassword;
+    if(typeof(hashedPassword)!==Boolean)
+    {
+      db.create(collection,data.body.phone,data.body,function(err)
+      {
+        callback(err);
+      });
+    }
+    else
+    {
+      callback(true);
+    }
+  }
+  else
+  {
     callback(true);
   }
 };
