996e9d377e46fd44069d97dc2c8b382cbe6de596
srihan93 <srihan93@gmail.com>
2021-06-03 22:19:23 +0530

    added order,cart and payment js - still pending work is there

--- apis/_order.js
+++ apis/_order.js
@@ -1,5 +1,19 @@
+const db = require("../lib/db");
 
+var _order = {};
+var collection = 'orders';
 
+_order.post = function(data,callback){
+    var cartId = data.body.phone;
+    db.read('carts',cartId,data,function(err,outData)
+    {
+        if(err===false)
+        {
+            //createOrderFromCart
+        }
+    });
+    
+}
 
 
 
