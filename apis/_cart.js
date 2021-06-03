894d89688cf3b731ebcb0def60b3186afdc55a4c
srihan93 <srihan93@gmail.com>
2021-06-03 19:18:11 +0530

    added cart method to create cart

--- apis/_cart.js
+++ apis/_cart.js
@@ -1,4 +1,40 @@
+const db = require("../lib/db");
+const validation = require("../lib/validation");
 
+var _cart ={};
+var collection ="carts";
+
+_cart.post = function(data,callback){
+    validation.addProductToCart(data.body,function(err)
+    {
+        let cartName;
+        if(err===false)
+        {
+            db.read('tokens',data.headers.token,data,function(err,userData){
+                if(err===false)
+                {
+                    cartName = userData["phoneNumber"];
+                    db.create(collection,cartName,data.body,function(err)
+                    {
+                        if(err===true)
+                        {
+                            callback(true);
+                        }
+                        else
+                        {
+                            callback(false);
+                        }
+                    });
+                }
+            });
+            
+        }
+        else{
+            callback(true);
+        }
+    });
+  
+}
 
 
 
