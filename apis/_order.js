ded5f82e3074adc57857620caaf4248129747e44
srihan93 <srihan93@gmail.com>
2021-06-05 00:02:34 +0530

    added logic for billing the cart

--- apis/_order.js
+++ apis/_order.js
@@ -1,21 +1,30 @@
 const db = require("../lib/db");
+const helpers = require("../lib/helpers");
+const email = require('./../lib/email');
+const payment = require('./../lib/payment');
 
 var _order = {};
-var collection = 'orders';
 
-_order.post = function(data,callback){
-    var cartId = data.body.phone;
-    db.read('carts',cartId,data,function(err,outData)
-    {
-        if(err===false)
-        {
-            //createOrderFromCart
+_order.post = function (data, callback) {
+  
+  db.read("tokens", data.headers.token, data, function (err, userData) {
+      if (err !== true) {
+          var cartId = userData.phoneNumber;
+      db.read("carts", cartId, data, function (err, cart) {
+        if (err === false) {
+          helpers.createOrder(cart,db.items,function (err) {
+              if(err!==false)
+              {
+                  callback(err);
+              }
+              callback(true);
+          });
         }
-    });
-    
-}
-
-
-
+        callback(true);
+      });
+    }
+    callback(true);
+  });
+};
 
 module.exports = _order;
