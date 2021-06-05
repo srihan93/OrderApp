2d7df0eaba3d6c16fa51aa955fda7c9b10d0553c
srihan93 <srihan93@gmail.com>
2021-06-05 18:09:05 +0530

    refactoring

--- constants/response.js
+++ constants/response.js
@@ -5,5 +5,30 @@ var response = {
     'statusCode':0
 }
 
+var result = 
+{
+    'success':false,
+    'message':'',
+    'data':{},
+    'statusCode':500
+}
+
+response.builder = function(success,message,data,statusCode)
+{
+    if(statusCode===200||statusCode===201)
+    {
+        result.success=true;
+        result.statusCode = statusCode;
+        result.data = data;
+        return result;
+    }
+    else{
+        result.success=false;
+        result.statusCode = statusCode;
+        result.message = message
+        return result;
+    }
+}
+
 
 module.exports = response;
